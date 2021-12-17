2258a085264d06c152f40c00e83978e200b94301
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-12-18 00:36:30 +0100

    Benchmark report creation in external tests

--- test/externalTests/ens.sh
+++ test/externalTests/ens.sh
@@ -69,6 +69,7 @@ function ens_test
 
     for preset in $SELECTED_PRESETS; do
         truffle_run_test "$config_file" "$BINARY_TYPE" "${DIR}/solc" "$preset" "${compile_only_presets[*]}" compile_fn test_fn
+        store_benchmark_report truffle ens "$preset" "${compile_only_presets[*]}"
     done
 }
 
